3a830c4ebc4cfc0c5891622336404c0b209765ec
Bdiebeak <123redcreeper123@gmail.com>
2020-07-01 22:35:49 +0400

    Реализовал прыжки и перемещение.
    Прыжки с замедлением времени и перемещение.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Параметры для движения")]
+    public float moveSpeed = 5f;
+
     [Header("Параметры для поворота игрока к прицелу")]
     public float rotationStep = 15f;
 
@@ -19,16 +24,31 @@ public class PlayerController : MonoBehaviour
     public Transform rightWrist = null;
     public Transform leftWrist = null;
 
+    [Header("Параметры для прыжка")]
+    public float jumpForce = 100f;
+    public GameObject mainPlayerCollider = null;
+    public GameObject jumpingPlayerCollider = null;
+    public LayerMask checkingLayers;
+    public float checkDistance = 1f;
+    public float delayBeforeCheck = 1f;
+    [Range(0, 1)]
+    public float slowedTime;
+
+    [Header("Параметры для стрельбы")]
     public Shooting shooting;
 
     private bool isRunning = false;
     private bool isAiming = false;
     private bool handsAreRaised = false;
+    private bool isGrounded = true;
+    private bool isStanding = true;
+    private bool isTryingToStand = false;
 
-    private float currentTimeForRisingHands;
-    private float currentArmsWeight;
+    private float currentTimeForRisingHands = 0f;
+    private float currentArmsWeight = 0f;
+    private float neededTimeForGroundCheck = 0f;
 
-    private Vector2 runDirection;
+    private Vector3 runDirection;
     private Vector3 crosshairPosition;
     private Vector3 crosshairWorldPositionWithOffsets;
     private Vector3 rightArmAimPosition;
@@ -37,6 +57,7 @@ public class PlayerController : MonoBehaviour
     private Camera mainCamera = null;
     private Animator playerAnimator = null;
     private Transform playerTransform = null;
+    private Rigidbody playerRigidbody = null;
 
     /// <summary>
     /// Инициализация параметров.
@@ -52,6 +73,7 @@ public class PlayerController : MonoBehaviour
         mainCamera = Camera.main;
         playerAnimator = GetComponent<Animator>();
         playerTransform = transform;
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     /// <summary>
@@ -59,10 +81,8 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        #region Обработка ввода игрока
-
         runDirection.x = Input.GetAxisRaw("Horizontal");
-        runDirection.y = Input.GetAxisRaw("Vertical");
+        runDirection.z = Input.GetAxisRaw("Vertical");
         runDirection = runDirection.normalized;
 
         isRunning = runDirection.magnitude == 0 ? false : true;
@@ -101,15 +121,52 @@ public class PlayerController : MonoBehaviour
         {
             isAiming = false;
         }
-        #endregion
+
+        // Прыжок или поднятие
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded && isStanding)
+            {
+                Jump();
+            }
+            else if (isGrounded && !isStanding && !isTryingToStand)
+            {
+                isTryingToStand = true;
+                GetUp();
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        RotatePlayerToCrosshair();
+        // Если игрок в полете - передает в его Animator параметры для
+        // корректного отоброжения положения тела в полете - 
+        // анимация разворачивается в ту же сторону, в которую игрок целится.
+        // --
+        // Если игрок на земле - разворачиваем его в сторону прицела и
+        // передаем параметры для корректного отображения анимации бега.
+        if (!isStanding && !isTryingToStand)
+        {
+            // Получаем направление взгляда камеры.
+            Vector3 aimDirection = mainCamera.transform.forward;
+            aimDirection.y = 0f;
+            aimDirection = aimDirection.normalized;
+            aimDirection = playerTransform.InverseTransformDirection(aimDirection);
 
-        playerAnimator.SetFloat("moveHorizontal", runDirection.x);
-        playerAnimator.SetFloat("moveVertical", runDirection.y);
+            playerAnimator.SetFloat("aimHorizontal", aimDirection.x);
+            playerAnimator.SetFloat("aimVertical", aimDirection.z);
+
+            CheckForGrounded();
+        }
+        else if (isGrounded && isStanding)
+        {
+            RotateToCrosshair();
+
+            playerAnimator.SetFloat("moveHorizontal", runDirection.x);
+            playerAnimator.SetFloat("moveVertical", runDirection.z);
+
+            playerRigidbody.velocity = playerTransform.TransformDirection(runDirection) * moveSpeed;
+        }
     }
 
     /// <summary>
@@ -119,7 +176,7 @@ public class PlayerController : MonoBehaviour
     {
         // Если игрок целится - ориентируем объекты запястьев по направлению к прицелу.
         // Вместе с запястьями будут вращаться и оружия.
-        if (isAiming)
+        if ((isAiming || !isStanding) && !isTryingToStand)
         {
             Vector3 rightDirection = crosshairWorldPositionWithOffsets - rightWrist.position;
             Vector3 leftDirection = crosshairWorldPositionWithOffsets - leftWrist.position;
@@ -138,7 +195,7 @@ public class PlayerController : MonoBehaviour
         playerAnimator.SetLookAtWeight(1.0f, 0.5f, 1.0f, 1.0f, 0.7f);
 
         // Вытягивание рук при прицеливании
-        if (isAiming)
+        if ((isAiming || !isStanding) && !isTryingToStand)
         {
             // Плавное поднятие рук
             handsAreRaised = true;
@@ -153,12 +210,6 @@ public class PlayerController : MonoBehaviour
                 currentTimeForRisingHands = timeForRaisingHands;
                 currentArmsWeight = armsWeight;
             }
-
-            playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, currentArmsWeight);
-            playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, currentArmsWeight);
-
-            playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, rightArmAimPosition);
-            playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftArmAimPosition);
         }
         else
         {
@@ -179,19 +230,19 @@ public class PlayerController : MonoBehaviour
                 currentArmsWeight = 0;
                 handsAreRaised = false;
             }
-
-            playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, currentArmsWeight);
-            playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, currentArmsWeight);
-
-            playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, rightArmAimPosition);
-            playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftArmAimPosition);
         }
+
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, currentArmsWeight);
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, currentArmsWeight);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, rightArmAimPosition);
+        playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftArmAimPosition);
     }
 
     /// <summary>
     /// Функция, вращения игрока к прицелу.
     /// </summary>
-    private void RotatePlayerToCrosshair()
+    private void RotateToCrosshair()
     {
         // Получаем текущий угол вращения камеры.
         float cameraYRotation = mainCamera.transform.rotation.eulerAngles.y;
@@ -200,5 +251,118 @@ public class PlayerController : MonoBehaviour
         playerTransform.rotation = Quaternion.Slerp(transform.rotation,
                                                     Quaternion.Euler(0f, cameraYRotation, 0f),
                                                     rotationStep * Time.fixedDeltaTime);
+    }
+
+    /// <summary>
+    /// Функций, отвечающая за прыжок игрока.
+    /// </summary>
+    private void Jump()
+    {
+        // Обнуляем скорость игрока, чтобы он не пролетал больше нужного
+        playerRigidbody.velocity = Vector3.zero;
+
+        // Изменяем статус игрока
+        isGrounded = false;
+        isStanding = false;
+
+        // Активируем коллайдер игрока в полете и деактивируем коллайдер для перемещения
+        mainPlayerCollider.SetActive(false);
+        jumpingPlayerCollider.SetActive(true);
+
+        // Запуска анимации
+        playerAnimator.SetTrigger("Jump");
+
+        // Физика прыжка
+        Vector3 jumpDirection;
+        if (runDirection.magnitude == 0)
+        {
+            jumpDirection = playerTransform.forward;
+        }
+        else
+        {
+            jumpDirection = playerTransform.TransformDirection(runDirection);
+        }
+        jumpDirection = jumpDirection.normalized;
+
+        playerRigidbody.AddForce((jumpDirection + Vector3.up) * playerRigidbody.mass * jumpForce);
+
+        // Разворот игрока по направлению прыжка
+        playerTransform.rotation = Quaternion.LookRotation(jumpDirection);
+
+        // Замедляем время.
+        SlowDownTime();
+
+        // Время, после которого будет осуществляться проверка на приземление
+        neededTimeForGroundCheck = Time.time + delayBeforeCheck;
+    }
+
+    /// <summary>
+    /// Находится ли игрок на земле.
+    /// </summary>
+    private void CheckForGrounded()
+    {
+        if (Time.time < neededTimeForGroundCheck) return;
+
+        CapsuleCollider playerCapsCollider = jumpingPlayerCollider.GetComponent<CapsuleCollider>();
+        Bounds playerCapsColliderBounds = playerCapsCollider.bounds;
+        Vector3 offsetVector = new Vector3(0f, 0f, playerCapsCollider.height / 2);
+
+        Ray[] checkingRays = {
+                                new Ray(playerCapsColliderBounds.center, Vector3.down),
+                                new Ray(playerCapsColliderBounds.center + offsetVector, Vector3.down),
+                                new Ray(playerCapsColliderBounds.center - offsetVector, Vector3.down),
+                             };
+
+        foreach (Ray currentRay in checkingRays)
+        {
+            isGrounded |= Physics.Raycast(currentRay, checkDistance, checkingLayers);
+        }
+
+        if (isGrounded)
+        {
+            NormalizeTime();
+        }
+    }
+
+    /// <summary>
+    /// Включает замедление времени.
+    /// </summary>
+    private void SlowDownTime()
+    {
+        Time.timeScale = slowedTime;
+    }
+
+    /// <summary>
+    /// Нормализирует время.
+    /// </summary>
+    private void NormalizeTime()
+    {
+        Time.timeScale = 1f;
+    }
+
+    /// <summary>
+    /// Функция, вызывающияся при вставании игрока.
+    /// </summary>
+    private void GetUp()
+    {
+        shooting.canShoot = false;
+        isAiming = false;
+
+        // Запуска анимации
+        playerAnimator.SetTrigger("GetUp");
+
+        // Активируем коллайдер игрока в полете и деактивируем коллайдер для перемещения
+        mainPlayerCollider.SetActive(true);
+        jumpingPlayerCollider.SetActive(false);
+    }
+
+    /// <summary>
+    /// Ивент для анимаций вставания.
+    /// </summary>
+    public void ChangeIsStandingFromAnimator()
+    {
+        isTryingToStand = false;
+        isStanding = true;
+        shooting.canShoot = true;
     }
 }
